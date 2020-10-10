fcbabd9df73d819dad629a129c2128b798ae1bde
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-10 17:18:24 +0530

    Program to demonstrate a sample program in c

---
+++ sample.c
@@ -0,0 +1,17 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate a sample program in c.
+Author  : Chpaone09®
+Date    : Oct 10, 2020
+
+************************************************************
+*/
+
+
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    printf("\n\n\t Hello World");
+}
